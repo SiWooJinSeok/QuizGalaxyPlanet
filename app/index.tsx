5a50117e116743468cd230dd4bdc358f8a8869db
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-06-25 19:04:41 +0900

    feat : project setting

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -5,6 +6,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>안녕!</Text>
+      <Link
+        style={styles.button}
+        href={{
+          pathname: "/login",
+        }}
+      >
+        <Text style={styles.buttonText}>로그인 페이지로</Text>
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    marginTop: 20,
+    padding: 20,
+    backgroundColor: "skyblue",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
   },
 });
