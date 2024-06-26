04bc1979d58c98909ff879c5fcd7463becc4aa2e
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-06-26 18:33:49 +0900

    refactor : add LoginForm component

---
+++ app/login/_components/LoginForm.tsx
@@ -0,0 +1,54 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import InputBox from "../../../assets/components/Input/InputBox";
+import { BASE_COLOR } from "../../../assets/constants/color";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitLogin = () => {
+    console.log(email, password);
+  };
+  return (
+    <View style={styles.form}>
+      <InputBox
+        labelName="이메일"
+        placeholder="이메일을 입력해주세요"
+        value={email}
+        onChangeText={(value: string) => setEmail(value)}
+        onSubmitEditing={submitLogin}
+      />
+      <InputBox
+        labelName="비밀번호"
+        placeholder="비밀번호를 입력해주세요"
+        value={password}
+        onChangeText={(value: string) => setPassword(value)}
+        onSubmitEditing={submitLogin}
+        secureTextEntry
+      />
+      <Pressable style={styles.submitButton} onPress={submitLogin}>
+        <Text style={styles.submitButtonText}>로그인</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 2,
+    justifyContent: "flex-start",
+  },
+  submitButton: {
+    backgroundColor: BASE_COLOR.LIGHT_PURPLE,
+    padding: 20,
+    borderRadius: 15,
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginTop: 30,
+  },
+  submitButtonText: {
+    fontSize: 20,
+    color: BASE_COLOR.LIGHT_YELLOW,
+  },
+});
--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,17 +1,9 @@
 import { Link } from "expo-router";
-import { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import InputBox from "../../assets/components/Input/InputBox";
+import { StyleSheet, Text, View } from "react-native";
 import { BASE_COLOR } from "../../assets/constants/color";
+import LoginForm from "./_components/LoginForm";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const submitLogin = () => {
-    console.log(email, password);
-  };
-
   return (
     <View style={styles.container}>
       <View style={styles.logo}>
@@ -23,26 +15,7 @@ export default function LoginPage() {
           <Text style={styles.buttonText}>홈으로</Text>
         </Link>
       </View>
-      <View style={styles.form}>
-        <InputBox
-          labelName="이메일"
-          placeholder="이메일을 입력해주세요"
-          value={email}
-          onChangeText={(value: string) => setEmail(value)}
-          onSubmitEditing={submitLogin}
-        />
-        <InputBox
-          labelName="비밀번호"
-          placeholder="비밀번호를 입력해주세요"
-          value={password}
-          onChangeText={(value: string) => setPassword(value)}
-          onSubmitEditing={submitLogin}
-          secureTextEntry
-        />
-        <Pressable style={styles.submitButton} onPress={submitLogin}>
-          <Text style={styles.submitButtonText}>로그인</Text>
-        </Pressable>
-      </View>
+      <LoginForm />
     </View>
   );
 }
@@ -60,21 +33,5 @@ const styles = StyleSheet.create({
   buttonText: {
     color: BASE_COLOR.LIGHT_YELLOW,
     fontSize: 30,
-  },
-  form: {
-    flex: 2,
-    justifyContent: "flex-start",
-  },
-  submitButton: {
-    backgroundColor: BASE_COLOR.LIGHT_PURPLE,
-    padding: 20,
-    borderRadius: 15,
-    alignItems: "center",
-    marginHorizontal: 20,
-    marginTop: 30,
-  },
-  submitButtonText: {
-    fontSize: 20,
-    color: BASE_COLOR.LIGHT_YELLOW,
   },
 });
